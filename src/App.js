28103915ede7ca81f97e90d0033c648ddaa2255f
EbbaEmanuelsson <ebbaemanuelsson97@gmail.com>
2023-10-07 21:35:05 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather react app</h1>
-      <footer>
-        This project was coded by Ebba Emanuelsson and is{" "}
-        <a
-          href="https://github.com/EbbaEmanuelsson/weather-react-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourecd on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather react app</h1>
+        <footer>
+          This project was coded by Ebba Emanuelsson and is{" "}
+          <a
+            href="https://github.com/EbbaEmanuelsson/weather-react-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourecd on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
