075f4ebe42cd2b54e1c331272c273962141558f3
EbbaEmanuelsson <ebbaemanuelsson97@gmail.com>
2023-10-25 19:04:24 +0200

    made the loop work so I can see all of the days of the forecast

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -3,18 +3,18 @@ import React from "react";
 
 export default function ForecastDay(props){
     function maxTemp() {
-        let temperature = Math.round(props.data[0].temperature.maximum);
+        let temperature = Math.round(props.data.temperature.maximum);
         return `${temperature}°`;
     }
 
     function minTemp() {
-        let temperature = Math.round(props.data[0].temperature.minimum);
+        let temperature = Math.round(props.data.temperature.minimum);
         return `${temperature}°`;
     }
 
     function day() {
-      let date = new Date(props.data[0].time * 1000);
-      let day = date.getDate();
+      let date = new Date(props.data.time * 1000);
+      let day = date.getDay();
 
       let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
@@ -26,8 +26,8 @@ export default function ForecastDay(props){
       <div className="ForecastDay">
         <div className="WeatherForecastDay">{day()}</div>
         <img
-          src={props.data[0].condition.icon_url}
-          alt={props.data[0].condition.description}
+          src={props.data.condition.icon_url}
+          alt={props.data.condition.description}
           className="WeatherForecastIcon"
         />
         <div>
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,9 +17,15 @@ export default function WeatherForecast(props){
     return (
       <div className="WeatherForecast">
         <div className="row mt-3 mb-3">
-          <div className="col">
-            <ForecastDay data={forecast}/>
-          </div>
+          {forecast.map(function (dailyForecast, index){
+            if (index < 5) {
+            return (
+             <div className="col" key={index}>
+              <ForecastDay data={dailyForecast} />
+             </div>
+            );
+           }
+          })}
         </div>
       </div>
     );
